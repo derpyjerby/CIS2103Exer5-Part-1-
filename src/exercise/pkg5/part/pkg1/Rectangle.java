18b00824d775cdff521cc658fda192f34476dbf5
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-25 23:05:17 -0700

    I commit.

---
+++ src/exercise/pkg5/part/pkg1/Rectangle.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercise.pkg5.part.pkg1;
+
+/**
+ *
+ * @author pardoje_cis21035
+ */
+public class Rectangle extends Shape{
+    
+}
