a96a547e8ffca57a250b6717deaa2650e29ac4a7
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-25 22:04:31 -0700

    I commit.

---
+++ src/exercise/pkg5/part/pkg1/Shape.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercise.pkg5.part.pkg1;
+
+/**
+ *
+ * @author pardoje_cis21035
+ */
+public class Shape {
+    String color;
+    boolean filled; 
+    
+    public Shape(){
+        this.color = "green";
+        this.filled = true;
+    }
+    
+    public Shape(String kulo, boolean tof){
+        this.color = kulo;
+        this.filled = tof;
+    }
+    
+    public getColor
+}
