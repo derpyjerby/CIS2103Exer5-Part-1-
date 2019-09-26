b4dd11f7cab2facfd51f7cf8dcf8a28de1c5280a
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-25 22:04:05 -0700

    I commit.

---
+++ src/exercise/pkg5/part/pkg1/Exercise5Part1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercise.pkg5.part.pkg1;
+
+/**
+ *
+ * @author pardoje_cis21035
+ */
+import java.util.Scanner;
+
+public class Exercise5Part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        Shape lol = new Shape();
+//        Shape lolz = new Shape( , );
+    }
+    
+}
