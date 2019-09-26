a244fff84a6ef1f7b172bd6d559f028804f3c987
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-25 23:47:30 -0700

    Final Circle Class

--- src/exercise/pkg5/part/pkg1/Circle.java
+++ src/exercise/pkg5/part/pkg1/Circle.java
@@ -12,4 +12,39 @@ package exercise.pkg5.part.pkg1;
  */
 public class Circle extends Shape{
     
+    double radius;
+    
+    public Circle (){
+        this.radius = 1.0;
+    }
+    
+    public Circle (double radius){
+        this.radius = radius;
+    }
+    
+    public Circle (double radius, String color, boolean filled){
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+    
+    public double getRadius() {        
+        return this.radius;
+    }
+    
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    public double getArea() {
+        return (Math.PI * Math.pow(radius, 2)); 
+    }
+    
+    public double getPerimeter() {
+        return (Math.PI * radius * 2); 
+    }
+    
+    public String toString () {
+        return String.format("A Circle with radius=%.2f, which is a subclass of %s", radius, super.toString());
+    }
 }
