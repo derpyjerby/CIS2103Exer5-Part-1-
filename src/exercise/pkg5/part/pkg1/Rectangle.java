3f896a1da386972766cd4dcca5edb4a11d1cb804
pardoje_cis21035 <pardoje_cis21035@ADMINRG-I180BTA.ceaccs.net>
2019-09-25 23:49:09 -0700

    Final Rectangle Class

--- src/exercise/pkg5/part/pkg1/Rectangle.java
+++ src/exercise/pkg5/part/pkg1/Rectangle.java
@@ -12,4 +12,51 @@ package exercise.pkg5.part.pkg1;
  */
 public class Rectangle extends Shape{
     
+    double width;
+    double length;
+    
+    public Rectangle (){
+        this.width = 1.0;
+        this.length = 1.0;
+    }
+    
+    public Rectangle (double width, double length){
+        this.width = width;
+        this.length = length;
+    }
+    
+    public Rectangle (double width, double length, String color, boolean filled){ 
+        this.width = width;
+        this.length = length;
+        this.color = color;
+        this.filled = filled;
+    }
+    
+    public double getWidth (){
+        return this.width;
+    }
+    
+    public double getLength (){
+        return this.length;
+    }
+    
+    public setWidth (double width){
+        this.width = width;
+    }
+    
+    public setLength (double length){
+        this.length = length;
+    }
+    
+    public double getArea (){
+        return this.length * this.width;
+    }
+    
+    public double getPerimeter (){
+        return 2 * (this.length + this.width);
+    }
+    
+    public String toString () {
+        return String.format("A Rectangle with width of=%.2f and length of =%.2f, which is a subclass of %s", width, length, super.toString());
+    }
 }
